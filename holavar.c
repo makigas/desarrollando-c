c8f07893142487287ce382b5c49b69a54bf33422
Dani Rodríguez <danirod@outlook.com>
2012-12-08 13:00:30 +0100

    holavar.c

---
+++ holavar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	
+	short operando1 = 6000, operando2 = 3500;
+	int suma = operando1 + operando2;
+	
+	printf("La suma da como resultado %d.\n", suma);
+	printf("%d + %d = %d\n", operando1, operando2, suma);
+	
+	printf("El 50%% de la gente opina que sí.\n");
+	
+	return 0;
+}
