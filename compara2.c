97cb201cee9c34e065c5e666d0e7f276b59a1b87
Dani Rodríguez <danirod@outlook.com>
2012-12-11 12:27:57 +0100

    compara2.c

---
+++ compara2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	
+	int a = 3, b = 4, c = 3;
+	
+	if((a == 5) && (b == 4)) {
+		printf("se cumple");
+	} else {
+		printf("no se cumple");
+	}
+	
+	printf("\nya está\n");
+	return 0;
+	
+	
+}
