e98508d9d45c51879e9feec54571ccdf0b5315b0
Dani Rodríguez <danirod@outlook.com>
2012-12-11 12:14:10 +0100

    comparacion.c

---
+++ comparacion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int a = 6, b = 4, c = 3;
+	
+	printf("voy a evaluar\n");
+	if(a == 5) {
+		printf("a vale 5\n");
+	} else if(a == 6) {
+		printf("pues a vale 6\n");
+	} else {
+		printf("a no vale ni 5 ni 6\n");
+	}
+	printf("he terminado de evaluar\n");
+	return 0;	
+}
